e4531a5182bb9d530720579cd149fcc3ce8666cc
initiald0824 <initiald0824@163.com>
2019-08-11 16:15:57 +0800

    *

--- basic/BinaryTree.py
+++ basic/BinaryTree.py
@@ -15,20 +15,60 @@ class BinaryTree(object):
     def __init__(self, node=None):
         self.root = node
 
+    # 增加新节点
     def add(self, item=None):
         node = TreeNode(item)
         if self.root is None:
             self.root = node
-        queue = list()
-        queue.append(self.root)
+        else:
+            queue = list()
+            queue.append(self.root)
+            while queue:
+                cur = queue.pop(0)
+                if cur.left is None:
+                    cur.left = node
+                    return
+                elif cur.right is None:
+                    cur.right = node
+                    return
+                else:
+                    queue.append(cur.left)
+                    queue.append(cur.right)
+
+    def pre_order(self):
+        res = []
+        if self.root is None:
+            return res
+        queue = []
+        p = self.root
+        queue.append(p)
         while queue:
-            cur = queue.pop(0)
-            if cur.left is None:
-                cur.left = node
-                return
-            elif cur.right is None:
-                cur.right = node
-                return
-            else:
-                queue.append(cur.left)
-                queue.append(cur.right)
+            p = queue.pop()
+            res.append(p.data)
+            if p.right:
+                queue.append(p.right)
+            if p.left:
+                queue.append(p.left)
+        return res
+
+    def in_order(self):
+        res = []
+        if self.root is None:
+            return res
+        queue = []
+        p = self.root
+        queue.append(p)
+
+
+if __name__ == '__main__':
+    D = TreeNode('D')
+    H = TreeNode('H')
+    K = TreeNode('K')
+    G = TreeNode('G', H, K)
+    F = TreeNode('F', G, None)
+    E = TreeNode('E', None, F)
+    C = TreeNode('C', D, None)
+    B = TreeNode('B', None, C)
+    A = TreeNode('A', B, E)
+    Tree = BinaryTree(A)
+    print(Tree.pre_order())
