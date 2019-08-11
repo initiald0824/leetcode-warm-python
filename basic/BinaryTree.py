d2a0df483b477cef1aa031107a014ea3e2ac3999
initiald0824 <initiald0824@163.com>
2019-08-11 13:57:33 +0800

    *

--- basic/BinaryTree.py
+++ basic/BinaryTree.py
@@ -14,4 +14,21 @@ class TreeNode(object):
 class BinaryTree(object):
     def __init__(self, node=None):
         self.root = node
-    
+
+    def add(self, item=None):
+        node = TreeNode(item)
+        if self.root is None:
+            self.root = node
+        queue = list()
+        queue.append(self.root)
+        while queue:
+            cur = queue.pop(0)
+            if cur.left is None:
+                cur.left = node
+                return
+            elif cur.right is None:
+                cur.right = node
+                return
+            else:
+                queue.append(cur.left)
+                queue.append(cur.right)
---
+++ ex.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author initiald0824
+# @Time 2019/8/11 13:45
+
+
