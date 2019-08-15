2a19997af75f00660808727046b81ea51be8d98f
initiald0824 <initiald0824@163.com>
2019-08-15 20:11:53 +0800

    *

--- 剑指offer/二叉树中和为某一值的路径.py
+++ 剑指offer/二叉树中和为某一值的路径.py
@@ -5,4 +5,24 @@
 
 # 输入一棵二叉树和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。
 # 从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
-# 例如下图中二叉树的和为22，则打印出两条路径，第一条
+# 例如下图中二叉树的和为22，则打印出两条路径，第一条路径包含结点10、12，
+# 第二条路径包含结点10、5、和 7。
+#        10
+#       /  \
+#      5   12
+#     /\
+#    4 7
+
+from basic.BinaryTree import TreeNode
+
+
+def get_tree_target_path(root):
+    pass
+
+
+def get_tree_target_path_func(node, path, res):
+    pass
+
+
+if __name__ == '__main__':
+    pass
